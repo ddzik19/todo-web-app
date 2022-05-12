2295c0378bd4222d6c497c80a37abb14fd1386fe
ddzik19 <58696796+ddzik19@users.noreply.github.com>
2022-05-12 11:45:25 +0100

    code refactor
    I have created a package that will generate a customized unique id. This will be used for profile id.
    
    Code has been refactored to implement my package.

--- note-app/routes/register.js
+++ note-app/routes/register.js
@@ -1,9 +1,10 @@
 const {
     request
 } = require('express');
-const userStore = require('../models/user-store.js');
-const store = require('../models/user-store.json');
-const users = store.users;
+const userStore = require('../models/user-store')
+const jsoning = require('jsoning');
+const db = new jsoning('user-store.json');
+const cuid = require('@devdamo/cuid');
 
 const reg = {
     signup(request, response) {
@@ -13,51 +14,31 @@ const reg = {
         response.render('signup', viewData);
     },
     register(req, res) {
-
         // generating teh id
-        var id = generateId();
+        var id = cuid(20, 1);
+        // since we are using jsoning we cant just get the 
+        var users = db.get("users");
         // validation, checking if this id already exists
         for (var i = 0; i < users.length; i++) {
-            if (users[i].id === id) {
-                id = generateId()
+            // if an id already exists we will generate a new id
+            // the loop will go on checking all id's in the database
+            if (db.get(id) === id) {
+                // generate a new id
+                id = cuid(20, 1);
             }
         }
-
         const newUser = {
             id: id,
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: req.body.password,
+            notes: []
         }
         userStore.addUser(newUser);
-        console.log(newUser);
+        // console.log(newUser);
         res.redirect('/');
     },
     authenticate(req, res) {},
 };
 
-function generateId() {
-    // generate the size of id
-    var max = 30;
-    var size = Math.floor(Math.random() * max)
-    // if the id size is less than 10 generate id again
-    while (size < 10) {
-        size = Math.floor(Math.random() * max)
-    }
-    console.log(size)
-    // creating a string for id
-    var stringId = ''
-
-    // generate the id
-    for (var i = 0; i < size + 1; i++) {
-        // generating a single digit number
-        var num = Math.floor(Math.random() * 10)
-        // adding the generated number to stringId
-        stringId += num;
-    }
-
-    return stringId;
-}
-
-// export the dashboard module
 module.exports = reg;
