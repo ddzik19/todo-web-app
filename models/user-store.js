204da931574cb00432d9e024f7f7d400fa04bec1
ddzik19 <ddzik09@gmail.com>
2022-05-23 11:57:50 +0100

    error cathcing
    Added error catching to user-store.js

--- models/user-store.js
+++ models/user-store.js
@@ -13,17 +13,25 @@ const userStore = {
         db.push("users", user);
     },
     getUserById(id) {
-        for (var i = 0; i < users.length; i++) {
-            if (users[i].id == id) {
-                return users[i];
+        try {
+            for (var i = 0; i < users.length; i++) {
+                if (users[i].id == id) {
+                    return users[i];
+                }
             }
+        } catch (err) {
+            console.log(err + ": in getUserById at line 15 in  user-store.js")
         }
     },
     getUserByEmail(email) {
-        for (var i = 0; i < users.length; i++) {
-            if (users[i].email == email) {
-                return users[i];
+        try {
+            for (var i = 0; i < users.length; i++) {
+                if (users[i].email == email) {
+                    return users[i];
+                }
             }
+        } catch (err) {
+            console.log(err + ": in getUserByEmail at line 22 user-store.js")
         }
     },
     addNote(note) {
@@ -37,17 +45,21 @@ const userStore = {
                 }
             }
         } catch (err) {
-            return console.log("Error in deleteNotes function at line 33")
+            return console.log(err + ": in deleteNotes function at line 33 user-store.js")
         }
     },
     getUserNotes(id) {
-        var unotes = []
-        for (var i = 0; i < notes.length; i++) {
-            if (notes[i].uid == id) {
-                unotes.push(notes[i]);
+        try {
+            var unotes = []
+            for (var i = 0; i < notes.length; i++) {
+                if (notes[i].uid == id) {
+                    unotes.push(notes[i]);
+                }
             }
+            return unotes;
+        } catch (err) {
+            console.log(err + ": in getUserNotes at line 45 user-store.js");
         }
-        return unotes;
     }
 }
 
